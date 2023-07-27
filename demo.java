e4330b4504d8114a649c01f4f5f412802b169137
Ramesh Basavraj Khed <rameshkhed3298@gmail.com>
2023-07-27 19:25:21 +0530

    Adding demo.java

---
+++ demo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class demo{
+    public static void main(String[]args){
+        int a=5;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println("Hello world");
+    
+        
+    }
+}
